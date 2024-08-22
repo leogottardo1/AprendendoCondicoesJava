8a37d38322c306eb0a5d58ca24ccdc1f1b72107b
DumbG1rlInLif3 <jennifer020605@edu.unipar.br>
2024-08-22 07:47:22 -0300

    Criação do projeto java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer idadePessoa = 20;
+        String nomePessoa = "Taffezinho";
+
+        /*
+        * Operadores Logicos
+        * > Maior
+        * >= Maior Igual
+        * < Menor
+        * <= Menor Igual
+        * == é igual
+        * != é diferente
+        * equal compara Textos
+         */
+        if (idadePessoa > 21) {
+            System.out.println("é maior que 21");
+
+        }else{
+            System.out.println("não é maior");
+        }
+
     }
 }
